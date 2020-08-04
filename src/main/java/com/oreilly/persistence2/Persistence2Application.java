7b69e7c00dda1c87499dbf2f69709ea960dce384
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-08-04 17:24:49 +0800

    initial commit

---
+++ src/main/java/com/oreilly/persistence2/Persistence2Application.java
@@ -0,0 +1,13 @@
+package com.oreilly.persistence2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Persistence2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Persistence2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/oreilly/persistence2/Persistence2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oreilly.persistence2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Persistence2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
